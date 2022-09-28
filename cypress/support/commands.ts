48a742a8fca4a87dc5b52b5ea4b3c3183019d2a2
Qinritukou <qinritukou@hotmail.com>
2022-09-28 21:21:07 +0900

    コメント追加

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,6 +2,7 @@ import { createPinia } from "pinia";
 import { mount } from "cypress/vue";
 import { h } from "vue";
 
+// 参考：https://docs.cypress.io/guides/component-testing/custom-mount-vue#Broken-Styles
 Cypress.Commands.add("mount", (component, ...args) => {
   args.global = args.global || {};
   args.global.plugins = args.global.plugins || [];
