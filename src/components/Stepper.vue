8388dd88f944b77eac29a40c8c33c905a5499031
Qinritukou <qinritukou@hotmail.com>
2022-09-28 20:37:00 +0900

    コンポーネントテスト機能を追加

---
+++ src/components/Stepper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button aria-label="decrement" @click="count--">-</button>
+    <span data-cy="counter">{{ count }}</span>
+    <button aria-label="increment" @click="count++">+</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const props = defineProps(["initial"]);
+
+const emit = defineEmits(["change"]);
+
+const count = ref(props.initial || 0);
+</script>
