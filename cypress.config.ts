0b3f03409db4a11bfb73892538e3409a6b12429f
Qinritukou <qinritukou@hotmail.com>
2022-10-03 17:10:09 +0900

    プロジェクトを再設定

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
-  },
-
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
